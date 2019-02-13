91549f984ecb832e9221ce78010d43beb72f8a37
REZA <hamidmick@yahoo.com>
2019-02-13 08:36:56 -0500

    comment

--- first/src/first/Test2.java
+++ first/src/first/Test2.java
@@ -5,5 +5,9 @@ public class Test2 {
 //njkdfjbdkfj
 	public void dir() {
 		System.out.println("tree merdi");
+		for (int i=0; i>10;i++) {
+			System.out.printf("yes",i);
+			//tree free
+		}
 	}
 }
